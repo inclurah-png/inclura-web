16a2ce65c4ed7daed1c54481e52de9df3320272b
inclurah-png <inclurah@gmail.com>
2026-05-25 20:10:18 +0100

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+function Login() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "420px",
+          background: "#0f172a",
+          padding: "40px",
+          borderRadius: "20px",
+        }}
+      >
+        <h1
+          style={{
+            color: "white",
+            marginBottom: "30px",
+            textAlign: "center",
+          }}
+        >
+          Login to Inclura
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          style={inputStyle}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          style={inputStyle}
+        />
+
+        <button style={buttonStyle}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const inputStyle = {
+  width: "100%",
+  padding: "16px",
+  marginBottom: "20px",
+  borderRadius: "12px",
+  border: "none",
+  fontSize: "16px",
+};
+
+const buttonStyle = {
+  width: "100%",
+  padding: "16px",
+  borderRadius: "12px",
+  border: "none",
+  background: "#38bdf8",
+  color: "white",
+  fontSize: "18px",
+  cursor: "pointer",
+};
+
+export default Login;
