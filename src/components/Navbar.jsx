111ff0590ae5f7c78c919dfb04d4f012219bf869
inclurah-png <inclurah@gmail.com>
2026-05-25 20:26:29 +0100

    Update Navbar.jsx
    Update Navbar routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 
 function Navbar() {
@@ -9,36 +10,64 @@ function Navbar() {
         alignItems: "center",
         padding: "20px 40px",
         background: "#020617",
+        borderBottom: "1px solid rgba(255,255,255,0.08)",
+        position: "sticky",
+        top: 0,
+        zIndex: 1000,
       }}
     >
       <Logo />
 
-      <div style={{ display: "flex", gap: "15px" }}>
-        <button
+      <div
+        style={{
+          display: "flex",
+          gap: "15px",
+          alignItems: "center",
+        }}
+      >
+        <Link
+          to="/login"
           style={{
-            padding: "12px 24px",
-            borderRadius: "10px",
-            border: "none",
-            cursor: "pointer",
-            fontSize: "16px",
+            textDecoration: "none",
           }}
         >
-          Login
-        </button>
+          <button
+            style={{
+              padding: "12px 24px",
+              borderRadius: "10px",
+              border: "none",
+              cursor: "pointer",
+              fontSize: "16px",
+              background: "white",
+              color: "#020617",
+              fontWeight: "600",
+            }}
+          >
+            Login
+          </button>
+        </Link>
 
-        <button
+        <Link
+          to="/signup"
           style={{
-            padding: "12px 24px",
-            borderRadius: "10px",
-            border: "none",
-            background: "#38bdf8",
-            color: "white",
-            cursor: "pointer",
-            fontSize: "16px",
+            textDecoration: "none",
           }}
         >
-          Sign Up
-        </button>
+          <button
+            style={{
+              padding: "12px 24px",
+              borderRadius: "10px",
+              border: "none",
+              background: "#38bdf8",
+              color: "white",
+              cursor: "pointer",
+              fontSize: "16px",
+              fontWeight: "600",
+            }}
+          >
+            Sign Up
+          </button>
+        </Link>
       </div>
     </nav>
   );
