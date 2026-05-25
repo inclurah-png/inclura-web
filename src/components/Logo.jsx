e86e7ffbf56d05ba0aa198ca80187ce4f1c2063e
inclurah-png <inclurah@gmail.com>
2026-05-25 17:45:53 +0100

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,33 @@
 function Logo() {
   return (
-    <h1
+    <div
       style={{
-        color: "#38bdf8",
-        fontSize: "52px",
-        fontWeight: "700",
-        margin: 0,
+        display: "flex",
+        alignItems: "center",
+        gap: "12px",
       }}
     >
-      Inclura
-    </h1>
+      <img
+        src="/logo.png"
+        alt="Inclura Logo"
+        style={{
+          width: "48px",
+          height: "48px",
+          borderRadius: "14px",
+        }}
+      />
+
+      <h1
+        style={{
+          color: "#38bdf8",
+          fontSize: "42px",
+          fontWeight: "700",
+          margin: 0,
+        }}
+      >
+        Inclura
+      </h1>
+    </div>
   );
 }
 
