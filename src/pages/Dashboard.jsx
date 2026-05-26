3eb4ea442f0a4dcd200642f03b44b083d36668b1
inclurah-png <inclurah@gmail.com>
2026-05-26 09:11:23 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,23 @@
+import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
+import Rightbar from "../components/Rightbar";
+
+function Dashboard() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        background: "#020617",
+        minHeight: "100vh",
+        color: "white",
+      }}
+    >
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
+  );
+}
+
+export default Dashboard;
 
