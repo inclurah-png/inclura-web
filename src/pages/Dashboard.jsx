47a168b52a60267b65ba91d0f2f87a5da431c888
inclurah-png <inclurah@gmail.com>
2026-05-26 14:26:10 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,31 +5,59 @@ import Feed from "../components/Feed";
 import Rightbar from "../components/Rightbar";
 
 function Dashboard() {
-  
-return (
-  <div
-    style={{
-      background: "#020617",
-      minHeight: "100vh",
-      color: "white",
-    }}
-  >
-    <Topbar />
-
+  return (
     <div
       style={{
-        display: "flex",
+        background: "#020617",
+        minHeight: "100vh",
+        color: "white",
+        paddingBottom: "90px",
       }}
     >
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-    </div>
-    <MobileNav />
-  </div>
-);
+      <Topbar />
 
+      {/* Main Layout */}
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+        }}
+      >
+        {/* Desktop Sidebar */}
+        <div
+          className="desktop-sidebar"
+          style={{
+            display: window.innerWidth > 900 ? "block" : "none",
+          }}
+        >
+          <Sidebar />
+        </div>
+
+        {/* Feed */}
+        <div
+          style={{
+            flex: 1,
+            width: "100%",
+          }}
+        >
+          <Feed />
+        </div>
+
+        {/* Desktop Rightbar */}
+        <div
+          className="desktop-rightbar"
+          style={{
+            display: window.innerWidth > 1100 ? "block" : "none",
+          }}
+        >
+          <Rightbar />
+        </div>
+      </div>
+
+      {/* Mobile Navigation */}
+      <MobileNav />
+    </div>
+  );
 }
 
 export default Dashboard;
-
