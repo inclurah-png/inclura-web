44fc5f55a88a310e1ddf55a84c763bc3cae3cf89
inclurah-png <inclurah@gmail.com>
2026-05-26 12:58:36 +0100

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,1 +1,23 @@
+function Avatar({ letter = "I", size = 50 }) {
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        borderRadius: "50%",
+        background: "#38bdf8",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "700",
+        fontSize: `${size / 2.2}px`,
+        color: "white",
+        flexShrink: 0,
+      }}
+    >
+      {letter}
+    </div>
+  );
+}
 
+export default Avatar;
