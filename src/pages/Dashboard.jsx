980237dd6b4b4131c04cbefb7986b0e990602c12
inclurah-png <inclurah@gmail.com>
2026-05-27 22:45:54 +0100

    Update Dashboard.jsx
    Update the dashboard for robust functions

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,63 +1,74 @@
-import MobileNav from "../components/MobileNav";
 import Topbar from "../components/Topbar";
+
 import Sidebar from "../components/Sidebar";
+
 import Feed from "../components/Feed";
+
 import Rightbar from "../components/Rightbar";
 
 function Dashboard() {
-  return (
-    <div
-      style={{
-        background: "#020617",
-        minHeight: "100vh",
-        color: "white",
-        paddingBottom: "90px",
-      }}
-    >
-      <Topbar />
 
-      {/* Main Layout */}
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-        }}
-      >
-        {/* Desktop Sidebar */}
-        <div
-          className="desktop-sidebar"
-          style={{
-            display: window.innerWidth > 900 ? "block" : "none",
-          }}
-        >
-          <Sidebar />
-        </div>
+return (
 
-        {/* Feed */}
-        <div
-          style={{
-            flex: 1,
-            width: "100%",
-          }}
-        >
-          <Feed />
-        </div>
+<div
+style={{
+background: "#020617",
+minHeight: "100vh",
+color: "white",
+}}
+>
 
-        {/* Desktop Rightbar */}
-        <div
-          className="desktop-rightbar"
-          style={{
-            display: window.innerWidth > 1100 ? "block" : "none",
-          }}
-        >
-          <Rightbar />
-        </div>
-      </div>
+<Topbar />
 
-      {/* Mobile Navigation */}
-      <MobileNav />
-    </div>
-  );
+<div
+style={{
+display: "flex",
+width: "100%",
+}}
+>
+
+{/* LEFT SIDEBAR */}
+
+<div
+style={{
+display:
+window.innerWidth > 900
+? "block"
+: "none",
+}}
+>
+<Sidebar />
+</div>
+
+{/* MAIN FEED */}
+
+<div
+style={{
+flex: 1,
+}}
+>
+<Feed />
+</div>
+
+{/* RIGHTBAR */}
+
+<div
+style={{
+display:
+window.innerWidth > 1100
+? "block"
+: "none",
+}}
+>
+<Rightbar />
+</div>
+
+</div>
+
+</div>
+
+);
+
 }
 
 export default Dashboard;
