545ef3ea57bd57eb2c02377aebab87ccc1333a6f
inclurah-png <inclurah@gmail.com>
2026-05-27 13:27:33 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+
+import {
+getAuth,
+GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+apiKey: "AIzaSyCb7YlZynAbMKjPWAwuOH61D4uUeAVtUlU",
+authDomain:
+"inclura-prod-90734.firebaseapp.com",
+projectId: "inclura-prod-90734",
+storageBucket:
+"inclura-prod-90734.firebasestorage.app",
+messagingSenderId: "694509989399",
+appId:
+"1:694509989399:web:dda8a2ba4cd25efd4af652",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const googleProvider =
+new GoogleAuthProvider();
+
+export default app;
