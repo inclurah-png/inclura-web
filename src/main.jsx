86bdf1edb097a5570fd572b82f9dcdd5b61a8bc3
inclurah-png <inclurah@gmail.com>
2026-05-27 21:39:17 +0100

    Update main.jsx
    Fix auth provider setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+
+import {
+BrowserRouter,
+} from "react-router-dom";
 
 import App from "./App";
 
-import { AuthProvider } from "./context/AuthContext";
+import {
+AuthProvider,
+} from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(
+document.getElementById("root")
+).render(
+<React.StrictMode> <BrowserRouter> <AuthProvider> <App /> </AuthProvider> </BrowserRouter>
+</React.StrictMode>
 );
