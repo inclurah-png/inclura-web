b95f71a53fb643c572bb29b091606c05114bead8
inclurah-png <inclurah@gmail.com>
2026-05-28 21:49:40 +0100

    Update main.jsx
    Fix main and add index css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+import "./index.css";
+
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(
