8eea157fff851c09169fa3159e3a262c808c02cf
inclurah-png <inclurah@gmail.com>
2026-05-28 19:54:19 +0100

    Update main.jsx
    Fix main.jsx auth import

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import "./index.css";
-
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(
