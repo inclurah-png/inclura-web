27b980f5c172a2821801137f81690d527b1331dd
inclurah-png <inclurah@gmail.com>
2026-05-28 16:33:52 +0100

    Update main.jsx
    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,31 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
-import {
-BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
+import "./index.css";
 
 import { AuthProvider }
 from "./contexts/AuthContext";
 
-
 ReactDOM.createRoot(
 document.getElementById("root")
 ).render(
-<React.StrictMode> <BrowserRouter> <AuthProvider> <App /> </AuthProvider> </BrowserRouter>
+
+<React.StrictMode>
+
+<BrowserRouter>
+
+<AuthProvider>
+
+<App />
+
+</AuthProvider>
+
+</BrowserRouter>
+
 </React.StrictMode>
+
 );
