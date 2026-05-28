ceaff92794b3b639abf51594f9752ebdf0057667
inclurah-png <inclurah@gmail.com>
2026-05-28 16:46:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,7 @@ import App from "./App";
 
 import "./index.css";
 
-import { AuthProvider }
-from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(
 document.getElementById("root")
