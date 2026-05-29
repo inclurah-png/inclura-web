26ce1dee28a2ab5ab1366bb3a5f0d75b5540e194
inclurah-png <inclurah@gmail.com>
2026-05-29 06:53:40 +0100

    Update firebase.js
    Fix firebase.js update

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,52 @@
+
 import { initializeApp } from "firebase/app";
 
 import {
-getAuth,
-GoogleAuthProvider,
+  getAuth,
+  GoogleAuthProvider,
 } from "firebase/auth";
 
+import {
+  getFirestore,
+} from "firebase/firestore";
 
-import { getFirestore } from "firebase/firestore";
+import {
+  getStorage,
+} from "firebase/storage";
 
 const firebaseConfig = {
-apiKey: "AIzaSyCb7YlZynAbMKjPWAwuOH61D4uUeAVtUlU",
-authDomain:
-"inclura-prod-90734.firebaseapp.com",
-projectId: "inclura-prod-90734",
-storageBucket:
-"inclura-prod-90734.firebasestorage.app",
-messagingSenderId: "694509989399",
-appId:
-"1:694509989399:web:dda8a2ba4cd25efd4af652",
+  apiKey:
+    "AIzaSyCb7YlZynAbMKjPWAwuOH61D4uUeAVtUlU",
+
+  authDomain:
+    "inclura-prod-90734.firebaseapp.com",
+
+  projectId:
+    "inclura-prod-90734",
+
+  storageBucket:
+    "inclura-prod-90734.firebasestorage.app",
+
+  messagingSenderId:
+    "694509989399",
+
+  appId:
+    "1:694509989399:web:dda8a2ba4cd25efd4af652",
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app =
+  initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const auth =
+  getAuth(app);
+
+export const db =
+  getFirestore(app);
+
+export const storage =
+  getStorage(app);
 
 export const googleProvider =
-new GoogleAuthProvider();
+  new GoogleAuthProvider();
 
 export default app;
