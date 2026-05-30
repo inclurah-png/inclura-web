a0a575cf7fac0581cbc0f731f4852d55e7eab033
inclurah-png <inclurah@gmail.com>
2026-05-30 05:59:05 +0100

    Create Messages.jsx
    Create Messages page

---
+++ src/pages/Messages.jsx
@@ -0,0 +1,34 @@
+
+function Messages() {
+
+return (
+
+<div
+style={{
+background: "#020617",
+minHeight: "100vh",
+padding: "24px",
+color: "white",
+}}
+>
+
+<h1>
+💬 Messages
+</h1>
+
+<p
+style={{
+color: "#94a3b8",
+marginTop: "12px",
+}}
+>
+Direct messaging system coming next.
+</p>
+
+</div>
+
+);
+
+}
+
+export default Messages;
