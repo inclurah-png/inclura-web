c3d02129fc37fed593c7b76ae3e9116767913af7
inclurah-png <inclurah@gmail.com>
2026-06-14 18:27:25 +0100

    Update App.jsx
    Fix and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,118 @@
 
+import { Routes, Route } from "react-router-dom";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Onboarding from "./pages/Onboarding";
+
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import Messages from "./pages/Messages";
+import SavedPosts from "./pages/SavedPosts";
+import Search from "./pages/Search";
+import UserProfile from "./pages/UserProfile";
+import Notifications from "./pages/Notifications";
+
+function App() {
+  return (
+    <Routes>
+
+      {/* PUBLIC ROUTES */}
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      <Route
+        path="/signup"
+        element={<Signup />}
+      />
+
+      <Route
+        path="/forgot-password"
+        element={<ForgotPassword />}
+      />
+
+      <Route
+        path="/onboarding"
+        element={<Onboarding />}
+      />
+
+      {/* PROTECTED ROUTES */}
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/edit-profile"
+        element={
+          <ProtectedRoute>
+            <EditProfile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/messages"
+        element={
+          <ProtectedRoute>
+            <Messages />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/saved-posts"
+        element={
+          <ProtectedRoute>
+            <SavedPosts />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/notifications"
+        element={
+          <ProtectedRoute>
+            <Notifications />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/user/:userId"
+        element={
+          <ProtectedRoute>
+            <UserProfile />
+          </ProtectedRoute>
+        }
+      />
+
+    </Routes>
+  );
+}
+
+export default App;
