f781471dd7ed33dfef63d6c268bae4bd86c1949f
inclurah-png <inclurah@gmail.com>
2026-06-16 05:51:00 +0100

    Update App.jsx
    Fix and update

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import SavedPosts from "./pages/SavedPosts";
 import Search from "./pages/Search";
 import UserProfile from "./pages/UserProfile";
 import Notifications from "./pages/Notifications";
+
 import AccessibilityHub from "./pages/AccessibilityHub";
 import OpportunitiesHub from "./pages/OpportunitiesHub";
 import CareGigs from "./pages/CareGigs";
@@ -26,182 +27,175 @@ import Enterprise from "./pages/Enterprise";
 import SOS from "./pages/SOS";
 
 function App() {
-  return (
-    <Routes>
+return (
+<Routes>
 
-      {/* PUBLIC ROUTES */}
-      <Route
-        path="/"
-        element={<Home />}
-      />
+  {/* PUBLIC ROUTES */}
+  <Route path="/" element={<Home />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/signup" element={<Signup />} />
+  <Route
+    path="/forgot-password"
+    element={<ForgotPassword />}
+  />
+  <Route
+    path="/onboarding"
+    element={<Onboarding />}
+  />
 
-      <Route
-        path="/login"
-        element={<Login />}
-      />
+  {/* MAIN DASHBOARD */}
+  <Route
+    path="/profile"
+    element={
+      <ProtectedRoute>
+        <Profile />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/signup"
-        element={<Signup />}
-      />
+  <Route
+    path="/edit-profile"
+    element={
+      <ProtectedRoute>
+        <EditProfile />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/forgot-password"
-        element={<ForgotPassword />}
-      />
+  <Route
+    path="/messages"
+    element={
+      <ProtectedRoute>
+        <Messages />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/onboarding"
-        element={<Onboarding />}
-      />
+  <Route
+    path="/notifications"
+    element={
+      <ProtectedRoute>
+        <Notifications />
+      </ProtectedRoute>
+    }
+  />
 
-      {/* PROTECTED ROUTES */}
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
+  <Route
+    path="/saved-posts"
+    element={
+      <ProtectedRoute>
+        <SavedPosts />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/edit-profile"
-        element={
-          <ProtectedRoute>
-            <EditProfile />
-          </ProtectedRoute>
-        }
-      />
+  <Route
+    path="/search"
+    element={
+      <ProtectedRoute>
+        <Search />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/messages"
-        element={
-          <ProtectedRoute>
-            <Messages />
-          </ProtectedRoute>
-        }
-      />
+  {/* HUBS */}
+  <Route
+    path="/accessibility"
+    element={
+      <ProtectedRoute>
+        <AccessibilityHub />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/saved-posts"
-        element={
-          <ProtectedRoute>
-            <SavedPosts />
-          </ProtectedRoute>
-        }
-      />
+  <Route
+    path="/opportunities"
+    element={
+      <ProtectedRoute>
+        <OpportunitiesHub />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/search"
-        element={
-          <ProtectedRoute>
-            <Search />
-          </ProtectedRoute>
-        }
-      />
+  <Route
+    path="/care-gigs"
+    element={
+      <ProtectedRoute>
+        <CareGigs />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-  path="/accessibility"
-  element={
-    <ProtectedRoute>
-      <AccessibilityHub />
-    </ProtectedRoute>
-  }
-/>
+  <Route
+    path="/mentor-hub"
+    element={
+      <ProtectedRoute>
+        <MentorHub />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-        path="/notifications"
-        element={
-          <ProtectedRoute>
-            <Notifications />
-          </ProtectedRoute>
-        }
-      />
+  <Route
+    path="/marketplace"
+    element={
+      <ProtectedRoute>
+        <Marketplace />
+      </ProtectedRoute>
+    }
+  />
 
-      <Route
-  path="/opportunities"
-  element={
-    <ProtectedRoute>
-      <OpportunitiesHub />
-    </ProtectedRoute>
-  }
-/>
+  {/* WALLET */}
+  <Route
+    path="/wallet"
+    element={
+      <ProtectedRoute>
+        <Wallet />
+      </ProtectedRoute>
+    }
+  />
 
-<Route
-  path="/care-gigs"
-  element={
-    <ProtectedRoute>
-      <CareGigs />
-    </ProtectedRoute>
-  }
-/>
+  <Route
+    path="/pay-topup"
+    element={
+      <ProtectedRoute>
+        <PayTopup />
+      </ProtectedRoute>
+    }
+  />
 
-<Route
-  path="/mentor-hub"
-  element={
-    <ProtectedRoute>
-      <MentorHub />
-    </ProtectedRoute>
-  }
-/>
+  {/* ENTERPRISE */}
+  <Route
+    path="/enterprise"
+    element={
+      <ProtectedRoute>
+        <Enterprise />
+      </ProtectedRoute>
+    }
+  />
 
-<Route
-  path="/marketplace"
-  element={
-    <ProtectedRoute>
-      <Marketplace />
-    </ProtectedRoute>
-  }
-/>
+  {/* SOS */}
+  <Route
+    path="/sos"
+    element={
+      <ProtectedRoute>
+        <SOS />
+      </ProtectedRoute>
+    }
+  />
 
-<Route
-  path="/wallet"
-  element={
-    <ProtectedRoute>
-      <Wallet />
-    </ProtectedRoute>
-  }
-/>
+  {/* USER PROFILE */}
+  <Route
+    path="/user/:userId"
+    element={
+      <ProtectedRoute>
+        <UserProfile />
+      </ProtectedRoute>
+    }
+  />
 
-<Route
-  path="/pay-topup"
-  element={
-    <ProtectedRoute>
-      <PayTopup />
-    </ProtectedRoute>
-  }
-/>
+</Routes>
 
-<Route
-  path="/enterprise"
-  element={
-    <ProtectedRoute>
-      <Enterprise />
-    </ProtectedRoute>
-  }
-/>
-
-<Route
-  path="/sos"
-  element={
-    <ProtectedRoute>
-      <SOS />
-    </ProtectedRoute>
-  }
-/>
-
-      <Route
-        path="/user/:userId"
-        element={
-          <ProtectedRoute>
-            <UserProfile />
-          </ProtectedRoute>
-        }
-      />
-
-    </Routes>
-  );
+);
 }
 
 export default App;
