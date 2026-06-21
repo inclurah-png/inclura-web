0592ef8eccec6f018a7361db9af3d928397dca6f
inclurah-png <inclurah@gmail.com>
2026-06-21 22:36:17 +0100

    Update CreatorVerificationPayment.jsx
    Fix and update

--- src/pages/payments/CreatorVerificationPayment.jsx
+++ src/pages/payments/CreatorVerificationPayment.jsx
@@ -1,1 +1,132 @@
 
+import { useState } from "react";
+import {
+  useFlutterwave,
+  closePaymentModal,
+} from "flutterwave-react-v3";
+
+function CreatorVerificationPayment() {
+  const [loading, setLoading] =
+    useState(false);
+
+  const config = {
+    public_key:
+      "FLWPUBK_TEST-1ee584892828ffa6942ef2e45a970768-X",
+
+    tx_ref:
+      Date.now().toString(),
+
+    amount: 5000,
+
+    currency: "NGN",
+
+    payment_options:
+      "card,banktransfer,ussd",
+
+    customer: {
+      email:
+        "test@inclura.com",
+
+      phone_number:
+        "08000000000",
+
+      name:
+        "Inclura User",
+    },
+
+    customizations: {
+      title:
+        "Creator Verification",
+
+      description:
+        "Pay for Creator Verification",
+
+      logo:
+        "",
+    },
+  };
+
+  const handleFlutterPayment =
+    useFlutterwave(config);
+
+  function payNow() {
+    setLoading(true);
+
+    handleFlutterPayment({
+      callback: (
+        response
+      ) => {
+        console.log(response);
+
+        alert(
+          "Payment Successful"
+        );
+
+        closePaymentModal();
+      },
+
+      onClose: () => {
+        setLoading(false);
+      },
+    });
+  }
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background:
+          "#020617",
+        color: "white",
+        display: "flex",
+        justifyContent:
+          "center",
+        alignItems:
+          "center",
+      }}
+    >
+      <div
+        style={{
+          width: "400px",
+          background:
+            "#0f172a",
+          padding: "30px",
+          borderRadius:
+            "20px",
+        }}
+      >
+        <h1>
+          Creator Verification
+        </h1>
+
+        <h2>
+          ₦5,000
+        </h2>
+
+        <button
+          onClick={payNow}
+          disabled={loading}
+          style={{
+            width: "100%",
+            padding: "15px",
+            border: "none",
+            borderRadius:
+              "12px",
+            background:
+              "#38bdf8",
+            color: "white",
+            fontWeight:
+              "700",
+          }}
+        >
+          {loading
+            ? "Loading..."
+            : "Pay Now"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreatorVerificationPayment;
+
