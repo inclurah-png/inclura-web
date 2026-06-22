3292c268483f682676782673c129d517dbd7664f
inclurah-png <inclurah@gmail.com>
2026-06-22 08:29:44 +0000

    Add Flutterwave integration and install dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -2,46 +2,22 @@ import { initializeApp } from "firebase/app";
 
 import {
   getAuth,
-  GoogleAuthProvider,
-} from "firebase/auth";
+    GoogleAuthProvider
+    } from "firebase/auth";
 
-import {
-  getFirestore,
-} from "firebase/firestore";
+    const firebaseConfig = {
+      apiKey: "AIzaSyCb7YlZynAbMKjPWAwuOH61D4uUeAVtUlU",
+        authDomain: "inclura-prod-90734.firebaseapp.com",
+          projectId: "inclura-prod-90734",
+            storageBucket: "inclura-prod-90734.firebasestorage.app",
+              messagingSenderId: "694509989399",
+                appId: "1:694509989399:web:dda8a2ba4cd25efd4af652"
+                };
 
-import {
-  getStorage,
-} from "firebase/storage";
+                const app = initializeApp(firebaseConfig);
 
-const firebaseConfig = {
-  apiKey:
-    "AIzaSyCb7YlZynAbMKjPWAwuOH61D4uUeAVtUlU",
+                export const auth = getAuth(app);
 
-  authDomain:
-    "inclura-prod-90734.firebaseapp.com",
+                export const googleProvider = new GoogleAuthProvider();
 
-  projectId:
-    "inclura-prod-90734",
-
-  storageBucket:
-    "inclura-prod-90734.firebasestorage.app",
-
-  messagingSenderId:
-    "694509989399",
-
-  appId:
-    "1:694509989399:web:dda8a2ba4cd25efd4af652",
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-
-export const db = getFirestore(app);
-
-export const storage = getStorage(app);
-
-export const googleProvider =
-  new GoogleAuthProvider();
-
-export default app;
+                export default app;
