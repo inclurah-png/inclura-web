01d5db2cd86a6ec07ff70c9a2b7e9637dfe99117
inclurah-png <inclurah@gmail.com>
2026-06-22 11:55:41 +0100

    Refactor firebase.js for improved readability
    Update

--- src/firebase.js
+++ src/firebase.js
@@ -9,22 +9,35 @@ import {
   getFirestore,
 } from "firebase/firestore";
 
+import {
+  getStorage,
+} from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCb7YlZynAbMKjPWAwuOH61D4uUeAVtUlU",
-  authDomain: "inclura-prod-90734.firebaseapp.com",
-  projectId: "inclura-prod-90734",
+  authDomain:
+    "inclura-prod-90734.firebaseapp.com",
+  projectId:
+    "inclura-prod-90734",
   storageBucket:
     "inclura-prod-90734.firebasestorage.app",
-  messagingSenderId: "694509989399",
+  messagingSenderId:
+    "694509989399",
   appId:
     "1:694509989399:web:dda8a2ba4cd25efd4af652",
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+  initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth =
+  getAuth(app);
 
-export const db = getFirestore(app);
+export const db =
+  getFirestore(app);
+
+export const storage =
+  getStorage(app);
 
 export const googleProvider =
   new GoogleAuthProvider();
