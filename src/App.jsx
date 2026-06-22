94d8c7db8c3354fc2b675d9c4c6d1a15599d20e2
inclurah-png <inclurah@gmail.com>
2026-06-22 08:15:33 +0100

    Update App.jsx
    Fix and update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Onboarding from "./pages/Onboarding";
 import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
 import VerificationCenter from "./pages/VerificationCenter";
-import CreatorVerificationPayment from "./pages/payments/CreatorVerificationPayment";
+import CreatorVerificationPayment from "./pages/CreatorVerificationPayment";
 import Messages from "./pages/Messages";
 import AdManager from "./pages/AdManager";
 import EnterpriseAds from "./pages/EnterpriseAds";
