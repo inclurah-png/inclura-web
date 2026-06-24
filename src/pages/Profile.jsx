f1cf11bb3900b68f211b2e2d7fdfff1d3977093c
inclurah-png <inclurah@gmail.com>
2026-06-24 18:37:47 +0100

    Add empty line for better code readability
    Update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+
 import { sendNotification } from "../utils/notificationHelper";
 import DashboardLayout from "../components/DashboardLayout";
 import ProfileHeader from "../components/ProfileHeader";
