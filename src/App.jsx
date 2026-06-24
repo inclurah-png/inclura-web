05826f22955f2c36f6369db5132c5800fd6beadb
inclurah-png <inclurah@gmail.com>
2026-06-24 15:04:24 +0100

    Add route for PremiumPayment component
    Fix and update

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ import CreatorAnalytics from "./pages/CreatorAnalytics";
 import EnterpriseCampaignManager from "./pages/EnterpriseCampaignManager";
 import EnterpriseAnalytics from "./pages/EnterpriseAnalytics";
 import PlatformAnalytics from "./pages/PlatformAnalytics";
+import PremiumPayment from "./pages/PremiumPayment";
 import PremiumDashboard from "./pages/PremiumDashboard";
 import PricingManager from "./pages/PricingManager";
 import PayTopup from "./pages/PayTopup";
@@ -345,6 +346,15 @@ return (
   }
 />
 
+  <Route
+  path="/premium-payment"
+  element={
+    <ProtectedRoute>
+      <PremiumPayment />
+    </ProtectedRoute>
+  }
+/>
+  
   <Route
   path="/premium-dashboard"
   element={
