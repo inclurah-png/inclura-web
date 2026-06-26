ae7295edc2c50749b7601382f9ab8e12c37ba640
inclurah-png <inclurah@gmail.com>
2026-06-26 18:51:41 +0100

    Add AccessibilityProvider to main.jsx
    Update

--- src/main.jsx
+++ src/main.jsx
@@ -11,15 +11,18 @@ import App from "./App";
 import "./index.css";
 
 import { AuthProvider } from "./context/AuthContext";
+import { AccessibilityProvider } from "./context/AccessibilityProvider";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+<React.StrictMode>
+<BrowserRouter>
+  <AuthProvider>
+    <AccessibilityProvider>
+      <App />
+    </AccessibilityProvider>
+  </AuthProvider>
+</BrowserRouter>
+</React.StrictMode>
 );
