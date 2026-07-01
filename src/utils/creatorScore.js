898d9dbb2546e18cac98fd5ebc32f7bdf4342a99
inclurah-png <inclurah@gmail.com>
2026-07-01 17:49:34 +0100

    Add creatorScore.js with post and community functions
    Fix and update

---
+++ src/utils/creatorScore.js
@@ -0,0 +1,13 @@
+export async function addVideoPost(uid) {}
+
+export async function addTextPost(uid) {}
+
+export async function addCrossPost(uid) {}
+
+export async function addComment(uid) {}
+
+export async function addReaction(uid, emoji) {}
+
+export async function updateCommunityTrust(uid) {}
+
+export async function checkPremiumQualification(uid) {}
