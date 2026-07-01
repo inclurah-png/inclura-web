3b67a9ef5403ef8862797284692c733d9771419b
inclurah-png <inclurah@gmail.com>
2026-07-01 17:52:17 +0100

    Implement creator score updates for post types
    Fix and update

--- src/utils/creatorScore.js
+++ src/utils/creatorScore.js
@@ -1,13 +1,34 @@
-export async function addVideoPost(uid) {}
+import {
+  doc,
+  updateDoc,
+  increment,
+} from "firebase/firestore";
 
-export async function addTextPost(uid) {}
+import { db } from "../firebase";
 
-export async function addCrossPost(uid) {}
+export async function addVideoPost(uid) {
+  await updateDoc(doc(db, "users", uid), {
+    "creatorEconomy.monthlyVideoPosts": increment(1),
+    "creatorEconomy.creatorScore": increment(4),
+  });
+}
 
-export async function addComment(uid) {}
+export async function addTextPost(uid) {
+  await updateDoc(doc(db, "users", uid), {
+    "creatorEconomy.monthlyTextPosts": increment(1),
+    "creatorEconomy.creatorScore": increment(2),
+  });
+}
 
-export async function addReaction(uid, emoji) {}
+export async function addCrossPost(uid) {
+  await updateDoc(doc(db, "users", uid), {
+    "creatorEconomy.monthlyCrossPosts": increment(1),
+    "creatorEconomy.creatorScore": increment(5),
+  });
+}
 
-export async function updateCommunityTrust(uid) {}
-
-export async function checkPremiumQualification(uid) {}
+export async function addComment(uid) {
+  await updateDoc(doc(db, "users", uid), {
+    "creatorEconomy.communityTrust": increment(2),
+  });
+}
