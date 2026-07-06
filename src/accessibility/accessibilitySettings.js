957cd17de5dadaad7aaaa70c480cd984305afe85
inclurah-png <inclurah@gmail.com>
2026-07-06 18:19:33 +0100

    Create defaultAccessibility object in accessibilitySettings
    Added default accessibility settings for various features.

--- src/accessibility/accessibilitySettings.js
+++ src/accessibility/accessibilitySettings.js
@@ -1,1 +1,25 @@
+export const defaultAccessibility = {
+  screenReader: false,
 
+  voiceNavigation: false,
+
+  liveCaptions: true,
+
+  textToSpeech: false,
+
+  highContrast: false,
+
+  dyslexiaMode: false,
+
+  cognitiveMode: false,
+
+  reducedMotion: false,
+
+  fontScale: 1,
+
+  preferredLanguage: "en",
+
+  autoTranslate: true,
+
+  autoPlayVoice: false,
+};
