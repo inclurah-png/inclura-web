bf99ae0c6fb8c8a5c73ff8aba63a3ddb9cff7122
inclurah-png <inclurah@gmail.com>
2026-07-06 18:22:01 +0100

    Implement AccessibilityEngine class with settings management
    Fix and update

--- src/accessibility/accessibilityEngine.js
+++ src/accessibility/accessibilityEngine.js
@@ -1,1 +1,36 @@
+import {
+  defaultAccessibility,
+} from "./accessibilitySettings";
 
+class AccessibilityEngine {
+
+  constructor() {
+    this.settings =
+      defaultAccessibility;
+  }
+
+  load(settings) {
+    this.settings = {
+      ...this.settings,
+      ...settings,
+    };
+  }
+
+  getSettings() {
+    return this.settings;
+  }
+
+  enable(feature) {
+    this.settings[feature] = true;
+  }
+
+  disable(feature) {
+    this.settings[feature] = false;
+  }
+
+}
+
+const accessibility =
+  new AccessibilityEngine();
+
+export default accessibility;
