313bd2d3e973c11b23cf20c24aec520e546fd291
inclurah-png <inclurah@gmail.com>
2026-07-06 18:26:57 +0100

    Add WCAG compliance checks object
    Update

--- src/accessibility/wcagCompliance.js
+++ src/accessibility/wcagCompliance.js
@@ -1,1 +1,23 @@
+export const WCAG = {
 
+  version: "2.2 AA",
+
+  checks: {
+
+    captions: true,
+
+    keyboardNavigation: true,
+
+    highContrast: true,
+
+    readableFonts: true,
+
+    scalableFonts: true,
+
+    screenReaderSupport: true,
+
+    multilingualSupport: true,
+
+  },
+
+};
