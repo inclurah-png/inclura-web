39c902b7b703490344b6a3943c1e6dc0f212b5ce
inclurah-png <inclurah@gmail.com>
2026-07-06 18:22:42 +0100

    Add speakText function for text-to-speech
    Update

--- src/accessibility/speechAccessibility.js
+++ src/accessibility/speechAccessibility.js
@@ -1,1 +1,15 @@
+import {
+  textToSpeech,
+} from "../providers/Piper";
 
+export async function speakText(
+  text,
+  language = "en"
+) {
+
+  return await textToSpeech({
+    text,
+    language,
+  });
+
+}
