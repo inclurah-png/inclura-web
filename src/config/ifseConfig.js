8d20540b1260c206b0b3cdd0736ab711df45a366
inclurah-png <inclurah@gmail.com>
2026-07-12 08:50:14 +0100

    Create IFSE_CONFIG with security features
    Added IFSE_CONFIG object with various security settings.

--- src/config/ifseConfig.js
+++ src/config/ifseConfig.js
@@ -1,1 +1,47 @@
+export const IFSE_CONFIG = {
 
+  engine: "Inclura Fortress Security Engine",
+
+  version: "1.0",
+
+  enabled: true,
+
+  auditLogs: true,
+
+  aiFraudDetection: true,
+
+  malwareScanning: true,
+
+  identityVerification: true,
+
+  accessibilityVerification: true,
+
+  paymentVerification: true,
+
+  documentVerification: true,
+
+  executiveApproval: true,
+
+  continuousMonitoring: true,
+
+  riskScoring: true,
+
+  threatIntelligence: true,
+
+  anomalyDetection: true,
+
+  accountProtection: true,
+
+  enterpriseProtection: true,
+
+  governmentProtection: true,
+
+  corporateProtection: true,
+
+  mediaProtection: true,
+
+  healthcareProtection: true,
+
+  accessibilityCertification: true,
+
+};
