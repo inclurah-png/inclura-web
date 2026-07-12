74399ad5a42dc8e14d68a7dafffaf258685f95f3
inclurah-png <inclurah@gmail.com>
2026-07-12 08:51:40 +0100

    Export additional modules from index.js
    Fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,7 @@
+export * from "./currency";
+export * from "./requestStatus";
+export * from "./verificationCategories";
+export * from "./ifse";
+export * from "./ifseConfig";
 
+export * from "./pricing";
