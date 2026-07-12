c9ceb287969412d62afbeb4e5d343db55f07c473
inclurah-png <inclurah@gmail.com>
2026-07-12 09:36:23 +0100

    Refactor exports and update VERIFICATION_PLANS

--- src/config/pricing/index.js
+++ src/config/pricing/index.js
@@ -1,41 +1,33 @@
-import creator from "./creator";
-import group from "./group";
-import organization from "./organization";
-import ngo from "./ngo";
-import institution from "./institution";
-import healthcare from "./healthcare";
-import media from "./media";
-import accessibility from "./accessibility";
-import addons from "./addons";
-import advertising from "./advertising";
-import partnerships from "./partnerships";
-import transactionFees from "./transactionFees";
+export { default as creator } from "./creator";
+export { default as group } from "./group";
+export { default as organization } from "./organization";
+export { default as ngo } from "./ngo";
+export { default as institution } from "./institution";
+export { default as healthcare } from "./healthcare";
+export { default as media } from "./media";
+export { default as accessibility } from "./accessibility";
 
-const VERIFICATION_PLANS = {
+export { default as religious } from "./religious";
+export { default as museum } from "./museum";
+export { default as tourism } from "./tourism";
+export { default as entertainment } from "./entertainment";
+
+export { default as addons } from "./addons";
+export { default as advertising } from "./advertising";
+export { default as partnerships } from "./partnerships";
+export { default as transactionFees } from "./transactionFees";
+
+export const VERIFICATION_PLANS = {
   creator,
   group,
   organization,
   ngo,
   institution,
   healthcare,
-  media,
+ media,
   accessibility,
+  religious,
+  museum,
+  tourism,
+  entertainment,
 };
-
-export {
-  creator,
-  group,
-  organization,
-  ngo,
-  institution,
-  healthcare,
-  media,
-  accessibility,
-  addons,
-  advertising,
-  partnerships,
-  transactionFees,
-  VERIFICATION_PLANS,
-};
-
-export default VERIFICATION_PLANS;
