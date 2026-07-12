0eb755af1fc0bde54b1890bb3afe4b39e211d1b2
inclurah-png <inclurah@gmail.com>
2026-07-12 13:30:43 +0100

    Add advertising campaign configurations

--- src/config/pricing/advertising.js
+++ src/config/pricing/advertising.js
@@ -1,1 +1,50 @@
+const advertising = [
+  {
+    id: "small",
+    name: "Small Campaign",
+    usd: 250,
+    duration: "7 Days",
+  },
 
+  {
+    id: "local",
+    name: "Local Campaign",
+    usd: 3500,
+    duration: "30 Days",
+    coverage: "1 State",
+  },
+
+  {
+    id: "regional",
+    name: "Regional Campaign",
+    usd: 24000,
+    duration: "60 Days",
+    coverage: "4 States",
+  },
+
+  {
+    id: "national",
+    name: "National Campaign",
+    usd: 80000,
+    duration: "90 Days",
+    coverage: "1 Country",
+  },
+
+  {
+    id: "continental",
+    name: "Continental Campaign",
+    usd: 500000,
+    duration: "6 Months",
+    coverage: "4 Countries",
+  },
+
+  {
+    id: "global",
+    name: "Global Campaign",
+    pricing: "Enterprise Negotiation",
+    suggestedBudgetUSD: 1000000000,
+    enterprise: true,
+  },
+];
+
+export default advertising;
