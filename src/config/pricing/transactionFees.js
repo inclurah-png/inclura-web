ada2b1b036aa83be524f8fd3519b52d3ff5afec0
inclurah-png <inclurah@gmail.com>
2026-07-12 10:07:54 +0100

    Add new transaction fees for various categories

--- src/config/pricing/transactionFees.js
+++ src/config/pricing/transactionFees.js
@@ -1,12 +1,20 @@
 const transactionFees = {
   eventTickets: 8,
+
   marketplace: 7,
+
   digitalProducts: 10,
+
   courses: 8,
+
   donations: 2,
+
   grants: 3,
+
   communityMembership: 8,
+
   sponsorship: 7,
+
   mentorship: 7,
 };
 
