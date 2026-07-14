7e8864c0bcb4ff9c44a4127dfd6e9a15f09e4409
inclurah-png <inclurah@gmail.com>
2026-07-14 17:24:20 +0100

    Create verificationPricing.js with organization pricing
    Added pricing information for various organization types.

---
+++ src/config/verificationPricing.js
@@ -0,0 +1,47 @@
+organization: {
+
+  business_name: {
+    yearlyUSD: 250,
+  },
+
+  startup: {
+    monthlyUSD: 35,
+    yearlyUSD: 420,
+  },
+
+  cooperative: {
+    yearlyUSD: 750,
+  },
+
+  charity: {
+    yearlyUSD: 600,
+  },
+
+  nonprofit: {
+    yearlyUSD: 600,
+  },
+
+  social_enterprise: {
+    yearlyUSD: 800,
+  },
+
+  small_limited_company: {
+    yearlyUSD: 1500,
+  },
+
+  medium_limited_company: {
+    pricing: "IFSE",
+    contractRequired: true,
+  },
+
+  large_limited_company: {
+    pricing: "IFSE",
+    contractRequired: true,
+  },
+
+  plc: {
+    pricing: "IFSE",
+    contractRequired: true,
+  },
+
+},
