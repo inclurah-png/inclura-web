f6afc6db0f4e1af946675526ba70ae3739ca3c7e
inclurah-png <inclurah@gmail.com>
2026-07-19 07:39:48 +0100

    Enhance Accessibility Runtime Engine with evaluations
    Added functions to evaluate various accessibility features and manage runtime state.

--- src/ifse/accessibility/AccessibilityRuntimeEngine.js
+++ src/ifse/accessibility/AccessibilityRuntimeEngine.js
@@ -1,3 +1,21 @@
+import { evaluateAccessibilityProfile } from "./AccessibilityProfileEngine";
+import { evaluateAccessibilityPreference } from "./AccessibilityPreferenceEngine";
+
+import { evaluateBlindSupport } from "./BlindSupportEngine";
+import { evaluateLowVision } from "./LowVisionEngine";
+import { evaluateColorBlindSupport } from "./ColorBlindSupportEngine";
+
+import { evaluateScreenReader } from "./ScreenReaderEngine";
+import { evaluateBraille } from "./BrailleEngine";
+
+import { evaluateVoiceNavigation } from "./VoiceNavigationEngine";
+import { evaluateKeyboardNavigation } from "./KeyboardNavigationEngine";
+
+import { evaluateHearingAccessibility } from "./HearingAccessibilityEngine";
+import { evaluateSpeechAccessibility } from "./SpeechAccessibilityEngine";
+
+import { evaluateMotorAccessibility } from "./MotorAccessibilityEngine";
+import { evaluateCognitiveAccessibility } from "./CognitiveAccessibilityEngine";
 // =======================================================
 // Inclura Fortress Security Engine (IFSE)
 // Accessibility Runtime Engine
@@ -16,6 +34,7 @@ let runtimeState = {
   lastUpdated: null,
 
 };
+const runtimeListeners = [];
 
 export function initializeAccessibilityRuntime(user) {
 
@@ -46,11 +65,38 @@ export function updateAccessibilityRuntime(update) {
     lastUpdated: new Date().toISOString(),
 
   };
-
+  notifyAccessibilityListeners();
+  
   return runtimeState;
 
 }
+export function subscribeAccessibility(listener) {
 
+  runtimeListeners.push(listener);
+
+  return () => {
+
+    const index = runtimeListeners.indexOf(listener);
+
+    if (index >= 0) {
+
+      runtimeListeners.splice(index, 1);
+
+    }
+
+  };
+
+}
+
+export function notifyAccessibilityListeners() {
+
+  runtimeListeners.forEach((listener) => {
+
+    listener(runtimeState);
+
+  });
+
+}
 export function shutdownAccessibilityRuntime() {
 
   runtimeState = {
@@ -66,7 +112,62 @@ export function shutdownAccessibilityRuntime() {
     lastUpdated: null,
 
   };
+return runtimeState;
+  }
 
+  export function loadAccessibilityModules(user) {
+
+  runtimeState.accessibilityProfile =
+    evaluateAccessibilityProfile(user);
+
+  runtimeState.activeModules = {
+
+    profile:
+      evaluateAccessibilityProfile(user),
+
+    preferences:
+      evaluateAccessibilityPreference(user),
+
+    blindSupport:
+      evaluateBlindSupport(user),
+
+    lowVision:
+      evaluateLowVision(user),
+
+    colorBlindSupport:
+      evaluateColorBlindSupport(user),
+
+    screenReader:
+      evaluateScreenReader(user),
+
+    braille:
+      evaluateBraille(user),
+
+    voiceNavigation:
+      evaluateVoiceNavigation(user),
+
+    keyboardNavigation:
+      evaluateKeyboardNavigation(user),
+
+    hearingAccessibility:
+      evaluateHearingAccessibility(user),
+
+    speechAccessibility:
+      evaluateSpeechAccessibility(user),
+
+    motorAccessibility:
+      evaluateMotorAccessibility(user),
+
+    cognitiveAccessibility:
+      evaluateCognitiveAccessibility(user),
+
+  };
+
+  runtimeState.lastUpdated =
+    new Date().toISOString();
+    
+notifyAccessibilityListeners();
+    
   return runtimeState;
 
-}
+  }
