168c6ca780078df754d4def68e9f8944aa22d6e2
inclurah-png <inclurah@gmail.com>
2026-08-09 15:35:33 +0100

    Add createEmergencyEscalation import to SOS.jsx
    Import createEmergencyEscalation from ifseEscalationEngine service.

--- src/pages/SOS.jsx
+++ src/pages/SOS.jsx
@@ -1,3 +1,6 @@
+import {
+  createEmergencyEscalation,
+} from "../services/ifseEscalationEngine";
 import { dispatchEmergency } from "../services/ifseDispatchEngine";
 import { useEffect, useState } from "react";
 import {
